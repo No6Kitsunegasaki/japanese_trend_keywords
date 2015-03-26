e264e553292fe54d512cd02ed9a2b5234ac1e001
No6Kitsunegasaki <none.gingerale@gmail.com>
2015-03-26 17:36:12 +0900

    use yaml

--- lib/japanese_trend_keywords.rb
+++ lib/japanese_trend_keywords.rb
@@ -3,18 +3,17 @@ require 'active_support'
 require 'active_support/core_ext'
 require "japanese_trend_keywords/version"
 require 'open-uri'
+require 'yaml'
 
 module JapaneseTrendKeywords
   class JapaneseTrendKeywords
     DEFAULT_NAME = :hatena
-    RSS_URLS = {
-      hatena: {url: 'http://d.hatena.ne.jp/hotkeyword?mode=rss', path: ['RDF', 'item']},
-      yahoo_burst: {url: 'http://searchranking.yahoo.co.jp/rss/burst_ranking-rss.xml', path: ['rss', 'channel', 'item']},
-    }.freeze
+    RSS_CONFIGS = YAML.load_file(File.expand_path('../services.yaml', __FILE__))
 
     def get(name)
       name = DEFAULT_NAME if name.nil?
-      generateGetResult(name, RSS_URLS[name][:url], RSS_URLS[name][:path])
+      config = RSS_CONFIGS[name]
+      generateGetResult(name, config[:url], config[:path])
     end
 
     def getAll
@@ -24,15 +23,25 @@ module JapaneseTrendKeywords
     private
 
     def generateGetResult(name, url, path)
+      rss = getRss(url)
       {
         :name => name,
+        :title => convertIntoTitle(rss, path[:title]),
         :url => url,
-        :keywords => convertIntoKeywords(getRss(url), path),
+        :keywords => convertIntoKeywords(rss, path[:item]),
       }
     end
 
     def getRss(uri)
       Hash.from_xml open(uri).read
+    end
+
+    def convertIntoTitle(hash, path)
+      title = hash
+      path.each do |i|
+        title = title[i]
+      end
+      title
     end
 
     def convertIntoKeywords(hash, path)
